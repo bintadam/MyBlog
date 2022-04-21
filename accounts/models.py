7f5c97fcb8652333f7fc706e68096b6bc8f1a631
zakiya adam <zakiyaadan12@gmail.com>
2022-04-21 22:21:23 +0300

    created class profile

--- accounts/models.py
+++ accounts/models.py
@@ -9,3 +9,12 @@ class User(AbstractUser):
     image = models.ImageField(upload_to='profile/', blank= True,null=True )
 
     REQUIRED_FIELDS =['email']
+
+
+class Profile(models.Model):
+    user = models.OneToOneField(User,on_delete=models.CASCADE,related_name='profile')
+    dob = models.DateField(null=True,blank=True)
+    image = models.ImageField(upload_to='profile/', blank= True,null=True )
+
+    def __str__(self):
+        return self.user.username    
