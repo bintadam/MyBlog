db492820040475ea54b8bbbf5c2bf57c9e029809
zakiya adam <zakiyaadan12@gmail.com>
2022-04-21 22:14:27 +0300

    created class user

--- accounts/models.py
+++ accounts/models.py
@@ -1,3 +1,11 @@
 from django.db import models
+from django.contrib.auth.models import AbstractUser
 
-# Create your models here.
+
+class User(AbstractUser):
+    email = models.EmailField(unique= True)
+    bio = models.CharField(max_length=500,blank=True)
+    Is_author = models.BooleanField(default=False)
+    image = models.ImageField(upload_to='profile/', blank= True,null=True )
+
+    REQUIRED_FIELDS =['email']
