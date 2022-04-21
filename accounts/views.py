f022442b504f3fc50960b2a4f93b8a61e0cdaeaa
zakiya adam <zakiyaadan12@gmail.com>
2022-04-21 22:22:35 +0300

    created view class signup

--- accounts/views.py
+++ accounts/views.py
@@ -1,6 +1,18 @@
 from django.shortcuts import render
-
+# from django.contrib.auth.forms import UserCreationForm
+from accounts.forms import Signupform
+from django.contrib.auth.mixins import LoginRequiredMixin
+from django.views.generic import CreateView,View
+from accounts.forms import UserEditForm,ProfileEditForm
+from django.views.generic.base import TemplateView
+from django.http import HttpResponse
 # Create your views here.
+class Signup_create_view(CreateView):
+    form_class = Signupform
+    template_name = 'account/signup.html'
+    success_url = '/blog'
+
+
 class HomePageView(TemplateView):
     template_name = 'account/home.html'
 
