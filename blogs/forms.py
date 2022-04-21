f2a469fbbf49eabfd4a478899134494c61fdd13b
zakiya adam <zakiyaadan12@gmail.com>
2022-04-21 22:47:16 +0300

    created search form

--- blogs/forms.py
+++ blogs/forms.py
@@ -18,3 +18,25 @@ class Contactform(forms.Form):
         phone = cleaned_data.get("phone")
         if email == '' and phone == '':
             raise forms.ValidationError("Atleast email or phone number should be provided" ,code= "invalid")
+
+
+class Postform(forms.ModelForm):
+    content = forms.CharField(widget=TinyMCE(attrs={'cols': 18, 'rows': 10}))
+    author = forms.CharField(disabled=True)
+    class Meta:
+        model = post
+        fields = ["title","content","status","category","image","date",]
+
+
+class Searchform(forms.Form):
+    search = forms.CharField(label='',widget=forms.TextInput(attrs={'class': 'special','placeholder':'search'}))
+    
+    # def clean_image(self):
+    #     image = self.cleaned_data.get('image')
+    #     if image.size > 200000:
+    #         raise forms.ValidationError('use low size image', code="invalid")
+    #     else:
+    #         return image
+
+
+
