f304cefd0c63345a60ad2256254ab84244d758f9
zakiya adam <zakiyaadan12@gmail.com>
2022-04-21 22:15:16 +0300

    created class Profileview

--- accounts/views.py
+++ accounts/views.py
@@ -3,3 +3,7 @@ from django.shortcuts import render
 # Create your views here.
 class HomePageView(TemplateView):
     template_name = 'account/home.html'
+
+
+class ProfileView(LoginRequiredMixin, TemplateView):
+    template_name = 'account/profile.html'    
