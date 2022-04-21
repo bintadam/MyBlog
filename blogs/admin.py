6b5f6fc53a151640206b239eef03a265c95e0520
zakiya adam <zakiyaadan12@gmail.com>
2022-04-21 22:49:58 +0300

    registered class post and category

--- blogs/admin.py
+++ blogs/admin.py
@@ -1,3 +1,7 @@
 from django.contrib import admin
+from blogs.models import post
+from blogs.models import Category
 
 # Register your models here.
+admin.site.register(post)
+admin.site.register(Category)
