b6715b8cb4ddcaca679779d6347b359ce74d1ba3
zakiya adam <zakiyaadan12@gmail.com>
2022-04-21 22:34:15 +0300

    added static folder

--- accounts/forms.py
+++ accounts/forms.py
@@ -22,4 +22,10 @@ class Signupform(UserCreationForm):
 class UserEditForm(forms.ModelForm):
     class Meta:
         model = User
-        fields = ('username','email','first_name','last_name')            
+        fields = ('username','email','first_name','last_name')           
+
+
+class ProfileEditForm(forms.ModelForm):
+    class Meta:
+        model = Profile
+        exclude = ('user',)         
